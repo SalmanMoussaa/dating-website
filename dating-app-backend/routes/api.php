17ac2b08da401e9cd1dd97d5eba6bcac2f5114b3
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-03-20 15:24:31 +0200

    feat(api.php): add routes for authentication and image upload

--- dating-app-backend/routes/api.php
+++ dating-app-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api'], function($router) {
+    Route::group(['prefix' => 'v0.0.1'], function () {
+        Route::group(['prefix' => 'auth'], function () {
+            Route::post('/login', [JWTController::class, "login"]);
+            Route::post('/register', [JWTController::class, "register"]);
+            Route::post('/forgotpassword', [JWTController::class, "forgotpassword"]);
+            Route::post('/addimage/{id}', [JWTController::class, "addimage"]);
+          
+        });
+    });
 });
