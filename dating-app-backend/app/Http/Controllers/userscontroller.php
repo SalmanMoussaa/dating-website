2a02b0019fe4c6237bfb27e3cadbfefaa6179030
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-03-20 20:55:47 +0200

    feat(userscontroller.php): add functions to get opposite gender users, get user by id, get sent and received messages, get blocked users, get liked users, edit user profile, and upload user image.

--- dating-app-backend/app/Http/Controllers/userscontroller.php
+++ dating-app-backend/app/Http/Controllers/userscontroller.php
@@ -151,5 +151,80 @@ class userscontroller extends Controller
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
     }
+    function getoppgender($id){
+        $user = User::find($id);
+        if($user->gender == "1"){
+            $oppositegender = "0";
+        }else{
+             $oppositegender = "1";
+        }
+        $users = User::where('gender',$oppositegender)->get();
+        return response()->json([
+            "users" => $users
+        ]);
+    }
+    // get user
+    function getuser($id){
+        $user = user::find($id);
+        return response()->json([
+            "user" => $user
+        ]);
+    }
+    // display sent and received messages
+    function getmessage($sender_id, $receiver_id){
+        $messages = Messages::where(function ($query) use ($sender_id, $receiver_id) {
+            $query->where('sender_id', $sender_id)
+                  ->where('receiver_id', $receiver_id);
+        })->orWhere(function ($query) use ($sender_id, $receiver_id) {
+            $query->where('sender_id', $receiver_id)
+                  ->where('receiver_id', $sender_id);
+        })->get();
+        
+        return response()->json([
+            "messages" =>  $messages
+        ]);
+    }
+    // display blocked users
+    function getblocks($sender_id, $receiver_id){
+        $block = blocks::where('sender_id', $sender_id)->where('receiver_id', $receiver_id)->get();
+        return response()->json([
+            "block" =>  $block
+        ]);
+    }
+    
+    // display liked
+    function getfavorites($sender_id, $receiver_id){
+       $favorites = favorites::where('sender_id',$sender_id)->where( 'receiver_id' ,$receiver_id)->get();
+       return response()->json(['favorites'=> $favorites]);
+    }
+    // edit profile 
+    function editprofile(Request $request)
+    {
+        $user = User::find($request->id);
+
+        $user->update([
+            "name" =>$request->name,
+            "email" => $request->email,
+            "age" => $request->age,
+            "location"=>$request->location,
+            "bio"=>$request->bio
+        ]);
+
+        return response()->json(['message' => 'User profile updated successfully', 'user' => $user]);
+    }
+    // upload image
+    function uploadImage(Request $request){
+        $encoded = $request->encoded;
+        $name = $request->name;
+
+        $decoded = base64_decode($encoded);
+
+        $file_path = public_path('images/'. $name . '.png');
+
+        file_put_contents($file_path,$decoded);
+
+        User::where("name",$name)->update("pic", "http://localhost/images" . $name . ".png");
+    }
 }
+
 
